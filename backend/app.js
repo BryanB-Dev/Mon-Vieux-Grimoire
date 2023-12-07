ef964645718d876a5dfc5e25abff6d33db7096c1
BryanB-Dev <perso703@pm.me>
2023-12-07 09:23:18 +0100

    Add dependencies and configure database connection

---
+++ backend/app.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DB_URI)
+   .then(()=>{
+      console.log("DB connection successful.");
+   })
+   .catch((err)=>{
+      console.log(`DB connection error:${err}`);
+   });
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(express.json());
+
+module.exports = app;
