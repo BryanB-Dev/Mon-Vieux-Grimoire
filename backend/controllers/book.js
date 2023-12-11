8f1f1c435a1c38b63503143dca2cee621de17491
BryanB-Dev <perso703@pm.me>
2023-12-11 13:56:45 +0100

    Add modifyBook route to book.js controller and routes

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -15,6 +15,28 @@ exports.createBook = (req, res) => {
         .catch(error => { res.status(400).json({ error }) })
 };
 
+exports.modifyBook = (req, res) => {
+    const bookObject = req.file ? {
+        ...JSON.parse(req.body.book),
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : { ...req.body };
+  
+    delete bookObject._userId;
+    Book.findOne({_id: req.params.id})
+        .then((book) => {
+            if (book.userId != req.auth.userId) {
+                res.status(401).json({ message : 'Not authorized'});
+            } else {
+                Book.updateOne({ _id: req.params.id}, { ...bookObject, _id: req.params.id})
+                .then(() => res.status(200).json({message : 'Objet modifié!'}))
+                .catch(error => res.status(401).json({ error }));
+            }
+        })
+        .catch((error) => {
+            res.status(400).json({ error });
+        });
+ };
+
 exports.getOneBook = (req, res) => {
     Book.findOne({
         _id: req.params.id
