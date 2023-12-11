e1afbea2bffe216554cee40f29908c13babd6fea
BryanB-Dev <perso703@pm.me>
2023-12-11 13:50:11 +0100

    Add getOneBook route and controller

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -15,7 +15,23 @@ exports.createBook = (req, res) => {
         .catch(error => { res.status(400).json({ error }) })
 };
 
-exports.getAllBooks = async (req, res) => {
+exports.getOneBook = (req, res) => {
+    Book.findOne({
+        _id: req.params.id
+    }).then(
+        (book) => {
+            res.status(200).json(book);
+        }
+    ).catch(
+        (error) => {
+            res.status(404).json({
+                error: error
+            });
+        }
+    );
+};
+
+exports.getAllBooks = (req, res) => {
     Book.find().then(
         (books) => {
             res.status(200).json(books);
--- backend/routes/book.js
+++ backend/routes/book.js
@@ -7,6 +7,7 @@ const multer = require('../middleware/multer-config');
 const bookCtrl = require('../controllers/book');
 
 router.post('/', auth, multer, bookCtrl.createBook);
+router.get('/:id', bookCtrl.getOneBook);
 router.get('/', bookCtrl.getAllBooks);
 
 module.exports = router;
