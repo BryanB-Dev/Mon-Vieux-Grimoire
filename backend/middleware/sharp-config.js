54d7b93d7a4586c043ec4742b6c2228be5c238d1
BryanB-Dev <perso703@pm.me>
2023-12-12 13:00:54 +0100

    Update dependencies and add image resize middleware

---
+++ backend/middleware/sharp-config.js
@@ -0,0 +1,33 @@
+const sharp = require('sharp');
+const fs = require('fs');
+
+module.exports = (req, res, next) => {
+
+    if (!req.file) {
+        return next()
+    }
+
+    const filePath = req.file.path;
+    const fileExt = req.file.filename.split('.').pop();
+    const fileName = req.file.filename.slice(0, -(fileExt.length + 1));
+
+    const webpFileName = `${fileName}.webp`;
+
+    sharp(filePath)
+        .resize({ width: 160, height: 240 })
+        .toFormat('webp')
+        .toFile(`images\\${fileName}.webp`)
+        .then(() => {
+            req.file.filename = webpFileName;
+
+            fs.unlink(`images/${fileName}.${fileExt}`, (err) => {
+                if (err) {
+                    console.error(err);
+                }
+                next();
+            });
+        })
+        .catch((error) => {
+            next(error);
+        });
+};
--- backend/routes/book.js
+++ backend/routes/book.js
@@ -3,11 +3,12 @@ const router = express.Router();
 
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
+const sharp = require('../middleware/sharp-config');
 
 const bookCtrl = require('../controllers/book');
 
-router.post('/', auth, multer, bookCtrl.createBook);
-router.put('/:id', auth, multer, bookCtrl.modifyBook);
+router.post('/', auth, multer, sharp, bookCtrl.createBook);
+router.put('/:id', auth, multer, sharp, bookCtrl.modifyBook);
 router.delete('/:id', auth, bookCtrl.deleteBook);
 router.get('/:id', bookCtrl.getOneBook);
 router.get('/', bookCtrl.getAllBooks);
